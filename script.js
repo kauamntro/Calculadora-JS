e84f54821eb76535d342ccc52f4991cd4c255e30
andersonezmonteiro <kaua1705monteiro@gmail.com>
2023-05-05 13:21:44 -0300

    Added the files

--- script.js
+++ script.js
@@ -1,22 +1,34 @@
-
-
-// Declaração dos botões com funções diferente de *Number*
-
-
 function colocarAlgo(num) {
-    var numero = document.getElementById('resultado').innerHTML;
+    const numero = document.getElementById('resultado').innerHTML;
+    if(numero.length >= 9) {
+        return
+    }
+    if(numero.length == 8) {
+    document.getElementById('resultado').innerHTML = numero + "...";
+    return
+    } else {
     document.getElementById('resultado').innerHTML = numero + num;
+    return
+    }
 }
 
 function limparDisplay() {
     document.getElementById('resultado').innerHTML = "";
 }
 
-function calcular(){
-    var resultado = document.getElementById('resultado').innerHTML;
+function calcular() {
+    const resultado = document.getElementById('resultado').innerHTML;
     if(resultado){
+        document.getElementById('conta').innerHTML = resultado;
         document.getElementById('resultado').innerHTML = eval(resultado);
     } else {
         document.getElementById('resultado').innerHTML = "Coloca algo!";
     }
+}
+
+function retirarUltimo() {
+    const resultado = document.getElementById('resultado').innerHTML;
+    const numero = resultado.substring(1);
+    document.getElementById('resultado').innerHTML = numero;
+    document.getElementById('conta').innerHTML = resultado;
 }
